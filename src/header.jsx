72b60818fa32a327ca6143b6ead8602640e0a7a2
AlexPerezGigante <perezgigantealex@fpllefia.com>
2024-03-29 22:44:29 +0100

    terminar responsive

--- src/header.jsx
+++ src/header.jsx
@@ -1,16 +1,17 @@
-
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem } from "@nextui-org/dropdown";
+import {Button, ButtonGroup} from "@nextui-org/button";
 import fotoPerfil from '/profile-pic-8.png'
 
 function Header() {
 
   return (
     <>
-    <header className=' font-source-sans3 h-24 flex justify-between shadow-lg fixed bg-fixed w-[100%] z-50 opacity-100 bg-opacity-100 bg-white'>
-        <div className='flex gap-3 ms-11 font-bold'>
-        <img src={fotoPerfil} alt="Foto perfil" className=' h-14 pb-2 mt-5' />
+    <header className=' font-source-sans3 h-20 lg:h-24 flex justify-between shadow-lg fixed bg-fixed w-[100%] z-50 opacity-100 bg-opacity-100 bg-white'>
+        <div className='flex gap-3 ms-4 lg:ms-11 font-bold'>
+        <img src={fotoPerfil} alt="Foto perfil" className='h-12 sm:lg-[3.25rem] lg:h-14 pb-2 mt-5' />
         <div className=' justify-start flex gap-3 text-slate-700 tracking-wider mt-8'>
             
-            <a href="" className= ' hover:text-violet-700 hover:transition-all hover:delay-0 transition-colors delay-200'>RAM MAHESHWARI</a>
+            <a href="" className= 'text-sm lg:text-base hover:text-violet-700 hover:transition-all hover:delay-0 transition-colors delay-200'>RAM MAHESHWARI</a>
 
         </div>
         </div>
@@ -22,10 +23,27 @@ function Header() {
             <a href="" className='hover:text-violet-700 hover:transition-all hover:delay-0  transition-colors delay-200'>PROJECTS</a>
             <a href="" className='hover:text-violet-700 hover:transition-all hover:delay-0  transition-colors delay-200'>CONTACT</a>
         </div>
+        
+        <Dropdown className="min-w-[500px] block sm:hidden  mt-[1.1rem] ps-[-300px] inset-x-0">
+      <DropdownTrigger className="block sm:hidden">
+        <Button data-hover="false"  className="bg-white mt-4 block sm:hidden">
+            Open Menu
+        </Button>
+        </DropdownTrigger>
+        <DropdownMenu className=" block sm:hidden text-right " aria-label="Static Actions">
+          <DropdownItem key="HOME"><a href="" className= ' hover:text-violet-700 hover:transition-all hover:delay-0 transition-colors delay-200 pe-60'>HOME</a></DropdownItem>
+          <DropdownItem key="ABOUT"><a href="" className='hover:text-violet-700 hover:transition-all hover:delay-0  transition-colors delay-200'>ABOUT</a></DropdownItem>
+          <DropdownItem key="PROJECTS"><a href="" className='hover:text-violet-700 hover:transition-all hover:delay-0  transition-colors delay-200'>PROJECTS</a></DropdownItem>
+          <DropdownItem key="CONTACT"><a href="" className='hover:text-violet-700 hover:transition-all hover:delay-0  transition-colors delay-200'>CONTACT</a></DropdownItem>
+        </DropdownMenu>
+        </Dropdown>
+        
+        
     </header>
-      
+    
     </>
   )
 }
+// block sm:hidden
 
 export default Header
